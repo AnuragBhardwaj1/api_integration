39638daa6ba2caf3d93d9b2b0dd7c1ffcf84374b
Anurag Bhardwaj <anuragbhardwaj.mait@gmail.com>
2016-11-03 17:32:27 +0530

    temp

---
+++ db/migrate/20161029172208_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :orders do |t|
+      t.string :status
+      t.string :cost
+      t.integer :pickup_id
+      t.integer :drop_id
+      t.decimal :estimated_pickup_time
+      t.decimal :estimated_delivery_time
+      t.string :name
+      t.decimal :age
+
+      t.timestamps null: false
+    end
+  end
+end
